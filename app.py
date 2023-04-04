666b6d3d30e54a58d83d0609ed91bafd9aa44a64
Mike <majweldon@users.noreply.huggingface.co>
2023-04-04 14:20:10 +0000

    Update app.py
    update pydub with pip install line

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import subprocess
 subprocess.run(["pip", "install", "openai"])
 subprocess.run(["pip", "install", "soundfile"])
+subprocess.run[("pip","install","pydub")]
 
 import os
 import openai
