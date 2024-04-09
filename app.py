e0bcafebbec6b93a93f9a091ba9fcdc9a203003b
Mike <majweldon@users.noreply.huggingface.co>
2024-04-09 12:38:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -132,8 +132,8 @@ def transcribe(audio, history_type):
 
 #Define Gradio Interface
 my_inputs = [
-    gr.Audio(source="microphone", type="filepath"), #Gradio 3.48.0
-    #gr.Audio(sources=["microphone"], type="filepath",format="wav"), #Gradio 4.x
+    #gr.Audio(source="microphone", type="filepath"), #Gradio 3.48.0
+    gr.Audio(sources=["microphone"], type="filepath",format="wav"), #Gradio 4.x
     #gr.Audio(sources=["microphone"],type="numpy"), #Gradio 4.x
     gr.Radio(["History","H+P","Impression/Plan","Full Visit","Handover","Psych","EMS","SBAR","Meds Only"], show_label=False),
 ]
